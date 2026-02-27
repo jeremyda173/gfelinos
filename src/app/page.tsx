125ffd5656f184b50b533ec6568cc8e87b111a0a
jeremyda173 <jeremygithud173@gmail.com>
2026-02-27 11:54:25 -0400

    Assemble Big Cats showcase in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,7 @@
 import dynamic from "next/dynamic";
 import Navbar from "@/components/ui/Navbar";
 import Hero from "@/components/sections/Hero";
-import Categories from "@/components/sections/Categories";
-import Featured from "@/components/sections/Featured";
-import Ecosystems from "@/components/sections/Ecosystems";
-import DynamicSpecies from "@/components/sections/DynamicSpecies";
-import Conservation from "@/components/sections/Conservation";
+import BigCats from "@/components/sections/BigCats";
 import Footer from "@/components/ui/Footer";
 
 // Lazy load components that are not needed immediately or are heavy
@@ -15,21 +11,15 @@ const Particles = dynamic(() => import("@/components/ui/Particles"), { ssr: fals
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen">
+    <main className="relative min-h-screen bg-black text-white">
       <Particles />
       <Navbar />
       
       <Hero />
       
-      <div className="relative z-10 bg-background">
-        <Categories />
-        <Featured />
-        <Ecosystems />
-        <DynamicSpecies />
-        <Conservation />
+      <div className="relative z-10">
+        <BigCats />
       </div>
-
-
 
       <Footer />
     </main>
